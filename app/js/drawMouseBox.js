ad39b62ad8c21080d39c4b3fe21a3ccd52beb260
Eric Denovellis <edeno@bu.edu>
2018-04-05 17:34:20 -0400

    Make tool tip more general

--- app/js/drawMouseBox.js
+++ app/js/drawMouseBox.js
@@ -42,20 +42,19 @@ function mouseBoxOver(d) {
     .style('left', (d3.event.pageX + 10) + 'px')
     .style('top', (d3.event.pageY + 10) + 'px')
     .html(function () {
-      return '<b>Trial ' + d.trial_id + '</b><br>' +
-             '<table>' +
-             '<tr><td>' + 'Rule:' + '</td><td><b>' + d.Rule + '</b></td></tr>' +
-             '<tr><td>' + 'Rule Repetition:' + '</td><td><b>' + d.Rule_Repetition + '</b></td></tr>' +
-             '<tr><td>' + 'Preparation Time:' + '</td><td><b>' + d.Preparation_Time + ' ms' + '</b></td></tr>' +
-             '<tr><td>' + 'Congruency:' + '</td><td><b>' + d.Current_Congruency + '</b></td></tr>' +
-             '<tr><td>' + 'Response Direction:' + '</td><td><b>' + d.Response_Direction + '</b></td></tr>' +
-             '<tr><td>' + 'Reaction Time:' + '</td><td><b>' + d.Reaction_Time + ' ms' + '</b></td></tr>' +
-             '<hr>' +
-             '<tr><td>' + 'Correct?:' + '</td><td><b>' + d.isCorrect + '</b></td></tr>' +
-             '<tr><td>' + 'Fixation Break?:' + '</td><td><b>' + d.Fixation_Break + '</b></td></tr>' +
-             '<tr><td>' + 'Error on previous trial?:' + '</td><td><b>' + d.Previous_Error + '</b></td></tr>' +
-             '<tr><td>' + 'Included in Analysis?:' + '</td><td><b>' + d.isIncluded + '</b></td></tr>' +
-             '</table>';
+      var table = '<b>Trial ' + d.trial_id + '</b><br>' + '<table>';
+      var varName = '';
+      for (varName in d) {
+        var isValid = (varName != 'trial_id') & (varName != 'spikes')
+          & (varName != 'sortInd');
+        if (isValid) {
+          table += '<tr><td>' + varName + ':' + '</td><td><b>' + d[varName]
+          + '</b></td></tr>';
+        }
+      }
+
+      table +=  '</table>';
+      return table;
     });
 
   d3.select(this)

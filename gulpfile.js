32cb765f3a73470a8279edf9ce5e3558cfc1f38c
Eric Denovellis <edeno@bu.edu>
2024-02-21 16:46:08 +0000

    Minor formatting

--- gulpfile.js
+++ gulpfile.js
@@ -21,19 +21,19 @@ var rollup = require('gulp-rollup');
  |--------------------------------------------------------------------------
  */
 gulp.task('createVendorJS', function () {
-   return gulp.src([
-     'app/components/jquery/dist/jquery.js',
-     'app/components/bootstrap/js/dropdown.js',
-     'app/components/d3/d3.js',
-     'app/components/queue-async/queue.js',
-     'app/components/colorbrewer/colorbrewer.js',
-     'app/components/spin.js/spin.js',
-     'app/components/d3-legend/d3-legend.js',
-     'app/components/fuse.js/src/fuse.js',
-   ]).pipe(concat('vendor.js'))
-     .pipe(gulp.dest('public/js'))
-     .pipe(connect.reload());
- });
+  return gulp.src([
+    'app/components/jquery/dist/jquery.js',
+    'app/components/bootstrap/js/dropdown.js',
+    'app/components/d3/d3.js',
+    'app/components/queue-async/queue.js',
+    'app/components/colorbrewer/colorbrewer.js',
+    'app/components/spin.js/spin.js',
+    'app/components/d3-legend/d3-legend.js',
+    'app/components/fuse.js/src/fuse.js',
+  ]).pipe(concat('vendor.js'))
+    .pipe(gulp.dest('public/js'))
+    .pipe(connect.reload());
+});
 
 gulp.task('createVendorJS-build', function () {
   return gulp.src([

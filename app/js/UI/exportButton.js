e6c1b055bee0b0a06b278c772f47962ebd766f4c
Eric Denovellis <edeno@bu.edu>
2025-06-23 12:41:44 -0400

    Revert "Update for canvas"
    This reverts commit fdbb12152c53390784d796ed08e9493f4afb21b9.

--- app/js/UI/exportButton.js
+++ app/js/UI/exportButton.js
@@ -1,41 +1,24 @@
 import rasterData from '../rasterData';
 import { save as saveSVG } from '../../../node_modules/d3-save-svg/index.js';
 
-const exportButton = d3.select('button#export');
+let exportButton = d3.select('button#export');
 
 exportButton
   .on('click', function () {
-    const svgs = d3.selectAll('#chart').selectAll('svg').nodes();
+    let svg = d3.selectAll('#chart').selectAll('svg')[0];
+    let levelNames = rasterData.rasterData().map(function (d) { return d.key; });
+    let curFactor = rasterData.curFactor();
+    let curEvent = rasterData.curEvent();
+    let neuronName = rasterData.neuronName();
 
-    svgs.forEach((svgNode, i) => {
-      const svgSelection = d3.select(svgNode);
-      // Find the canvas associated with this SVG
-      const canvas = svgSelection.select('.spikes-canvas').node();
-      if (!canvas) return;
-
-      // Get the canvas content as a base64 image URL
-      const imgData = canvas.toDataURL('image/png');
-
-      // Temporarily embed the canvas image into the SVG for export.
-      // We insert it before '.smoothLine' to ensure it's under the smoothing line.
-      const tempImage = svgSelection.select('g').insert('image', '.smoothLine')
-        .attr('xlink:href', imgData)
-        .attr('width', canvas.width)
-        .attr('height', canvas.height);
-
-      // Prepare the filename (logic from original file)
-      const levelNames = rasterData.rasterData().map(function (d) { return d.key; });
-      const curFactor = rasterData.curFactor();
-      const curEvent = rasterData.curEvent();
-      const neuronName = rasterData.neuronName();
-      const saveName = `${neuronName}_${curEvent}_${curFactor}_${levelNames[i]}`;
-
-      // Save the modified SVG
-      saveSVG(svgNode, { filename: saveName });
-
-      // IMPORTANT: Remove the temporary image after saving to restore the live chart
-      tempImage.remove();
+    svg.forEach(function (s, i) {
+      let saveName = neuronName + '_' +
+        curEvent + '_' +
+        curFactor + '_' +
+        levelNames[i];
+      saveSVG(s, { filename: saveName });
     });
+
   });
 
 export default exportButton;

a40f92c0942e97a975186b46fe515a6eed3c1cf9
Eric Denovellis <edeno@bu.edu>
2025-06-23 08:09:35 -0400

    Fix the Circular Dependency

--- app/js/chartHeight.js
+++ app/js/chartHeight.js
@@ -1,5 +1,4 @@
 import rasterView from './rasterView';
-import rasterData from './rasterData';
 /* If showing spikes, set to reasonable height so spikes are visible, else
    pick the normal height of the plot if it's small or a value that can fit
    several plots on the screen.
@@ -9,6 +8,6 @@ export default function (data) {
   const noSpikesHeight = 250;
   let heightMargin = rasterView.margin().top + rasterView.margin().bottom;
   let withSpikesHeight = (data.values.length * spikeDiameter) + heightMargin;
-
-  return rasterData.showSpikes() ? withSpikesHeight : d3.min([noSpikesHeight, withSpikesHeight]);
+  const shouldShowSpikes = rasterView.showSpikes();
+  return shouldShowSpikes ? withSpikesHeight : d3.min([noSpikesHeight, withSpikesHeight]);
 }

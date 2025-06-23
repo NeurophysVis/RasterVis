03ef984b7115161f326e9c9a500fd6374a393cd2
Eric Denovellis <edeno@bu.edu>
2025-06-23 10:05:43 -0400

    Reapply "Reduce Redundant DOM Queries"
    This reverts commit f8b10c6d4d25c430611899e8d54a2ce41025e457.

--- app/js/UI/brainAreaCheckboxes.js
+++ app/js/UI/brainAreaCheckboxes.js
@@ -4,14 +4,7 @@ import rasterData from '../rasterData';
 let brainAreaCheckboxes = createBrainAreaCheckboxes();
 
 brainAreaCheckboxes.on('change', function () {
-  var choices = [];
-  d3.selectAll('.checkbox').each(function (d) {
-    let cb = d3.select(this).select('input');
-    if (cb.property('checked')) {
-      choices.push(cb.property('id'));
-    }
-  });
-
+  const choices = d3.selectAll('#NeuronFilter .checkbox input:checked').nodes().map(cb => cb.id);
   rasterData.includeBrainAreas(choices);
 });
 
